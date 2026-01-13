900452e45ac034d77c4edbd7dde64078cfcb1370
Kingvics <okoromivictorsunday@gmail.com>
2026-01-13 08:16:15 +0100

    week2 resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+         Job job1 =  new Job();
+         job1._jobTitle = "Software Engineer";
+         job1._company = "Microsoft";
+         job1._startYear = 2020;
+         job1._endYear = 2026;
+
+         Job job2 = new Job();
+         job2._jobTitle = "Data Analyst";
+         job2._company = "Facebook";
+         job2._startYear = 2023;
+         job2._endYear = 2026;
+
+        // job1.Display();
+        // job2.Display();
+
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Victor Resume";
+        myResume._job.Add(job1);
+        myResume._job.Add(job2);
+
+        myResume.Display();
+       
     }
 }
